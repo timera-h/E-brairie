b4ad399971ef062ffc4fc31973b7f75ee902d2c9
clementRbl <clement.rbl@gmail.com>
2021-04-20 11:59:22 +0200

    add test connection and .envExemple

--- src/config/database.js
+++ src/config/database.js
@@ -3,5 +3,9 @@ import config from './env';
 
 const sequelize = new Sequelize(config.db_name, config.db_user, config.db_password, {dialect: "mysql"});
 const db = {sequelize, Sequelize}
+sequelize.authenticate()
+.then(() => {
+    console.log('connecté ');
+})
 
 export default db;
