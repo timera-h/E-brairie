d9caa130b4f8d1a5bfabd7f7bc05f3b550d88011
BrunoMonm <massurvive@gmail.com>
2021-04-21 12:20:51 +0200

    models added to index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,21 @@
 import accountRouter from './Account';
 import authorRouter from './Author';
 import profileRouter from './Profile';
+import accountDao from './Account/dao';
+import authorDao from './Author/dao';
+import profileDao from './Profile/dao';
 
 const routes = [accountRouter, authorRouter, profileRouter];
 
-export default routes;
+const models = {
+    accountDao,
+    authorDao,
+    profileDao
+}
+
+
+module.exports = {routes, models};
+// const test = [routes, models];
+
+// export default test;
+ 
